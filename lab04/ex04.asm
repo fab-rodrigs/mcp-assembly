f1053219dba4f95629112e7a200457fbd09ff7d2
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2023-10-06 14:21:32 -0300

    Create ex04.asm

---
+++ lab04/ex04.asm
@@ -0,0 +1,11 @@
+##############################################################################
+#
+#     Utilizando os procedimentos implementados no exercício anterior
+# implemente um programa que solicita ao usuário uma frase, e em seguida,
+# apresenta apenas a primeira palavra da frase digitada com todas as letras
+# maiúsculas.
+#
+#     Exemplo, se o usuãrio digitar "Bom dia!", o programa deverá 
+# imprimir "BOM"
+#
+##############################################################################
