d78f77309d0d1e87aa079be3ecbc242c0992cb13
Fabricio Rodrigues <76504799+fab-rodrigs@users.noreply.github.com>
2023-11-05 18:36:20 -0300

    Create lab05_b.asm

---
+++ lab05/lab05_b.asm
@@ -0,0 +1,30 @@
+#########################################################
+# Laboratório 03 - MCP22105
+# Procedimentos
+#
+# Aluno: 
+#########################################################
+
+#########################################################
+# Um número é dito quadrado perfeito, se puder ser escrito 
+# como o quadrado de um número natural (Ex. 1, 4, 9, 16, 25). 
+# É possível (embora não muito prático) calcular o quadrado 
+# perfeito de um número natural de forma recursiva, conforme 
+# o algoritmo abaixo:
+#
+# int q_perfeito(int n){ 
+#     if(n == 0) {
+#         return n; 
+#     } else {
+#         return q_perfeito(n-1) + 2*n - 1; 
+#     }
+# }
+#
+# Implemente a função do quadrado perfeito apresentada acima, 
+# e faça um programa que irá apresentar os valores do quadrado 
+# perfeito dos primeiros 10 números naturais. Utilize as chamadas 
+# de sistema para a entrada e saída de dados. O código deve ser 
+# implementado seguindo a convenção de chamada de procedimento 
+# estudada em sala de aula.
+#########################################################
+
